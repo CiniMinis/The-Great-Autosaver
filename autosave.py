103300c66f269d17f2b92a400d0697543dd9a1bf
unknown <temp18@ROTBERG.LOCAL>
2018-01-31 16:02:13 +0200

    added documentation and basic gui customization

--- autosave.py
+++ autosave.py
@@ -2,33 +2,54 @@
     The Great Autosaver
     by Jonathan Kalish
 """
-from pyautogui import hotkey
+import pyautogui
 import time
 
-TIME_TO_WAIT = 30
-YES_ANSWERS = ['YES', 'Y']
+DEFAULT_TIME_TO_WAIT = 60
+YES_ANSWER = "Yes"
+NO_ANSWER = "No"
+MAIN = '__main__'
 
 
 def save():
     """
         saves with ctrl s
     """
-    hotkey('ctrl', 's')
+    pyautogui.hotkey('ctrl', 's')
 
 
-def eclipse_save():
+def save_all():
     """
-        saves all in eclipse with ctrl shift s
+        saves all with ctrl shift s
     """
-    hotkey('ctrl', 'shift', 's')
+    pyautogui.hotkey('ctrl', 'shift', 's')
 
 
-reply = raw_input("Do you want to save-all? (ctrl+shift+s). reply yes/y to save-all")
+def main():
+    """
+        main method
+        handles customization
+    """
+    # asks if you want to use save-all
+    save_all_reply = pyautogui.confirm(text='Do you want to use save-all? (ctrl + shift + s)',
+                                       title='Save All', buttons=[YES_ANSWER, NO_ANSWER])
+    # gets the save interval
+    time_to_wait = pyautogui.prompt(text='How long to wait between saves? (in seconds, default = 60)',
+                                    title='Save Interval', default='')
+    # if cancel was pressed
+    if (time_to_wait is None) or (time_to_wait == ''):
+        time_to_wait = DEFAULT_TIME_TO_WAIT
 
-while True:
-    time.sleep(TIME_TO_WAIT)
-    print "Saving!"
-    if reply.upper() in YES_ANSWERS:
-        eclipse_save()
-    else:
-        save()
+    # forever
+    while True:
+        # wait time given
+        time.sleep(float(time_to_wait))
+        print "Saving!"
+        # checks if to save_all
+        if save_all_reply == YES_ANSWER:
+            save_all()
+        else:
+            save()
+
+if __name__ == MAIN:
+    main()
